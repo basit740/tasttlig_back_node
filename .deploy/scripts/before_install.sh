5af3f48329c6a494efdde43b49a23ff1828f937f
Slingr <liquidd2@gmail.com>
2020-06-06 20:48:49 -0400

    fix failing deploy

--- .deploy/scripts/before_install.sh
+++ .deploy/scripts/before_install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Install node.js and Forever.js
 sudo yum update
+sudo curl -sL https://rpm.nodesource.com/setup_10.x | sudo bash -
 sudo yum install nodejs -y
 sudo npm install forever -g
