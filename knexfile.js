4c105f0fbb805373d5fbd928af78ca0e1f98664c
Slingr <liquidd2@gmail.com>
2020-06-07 22:50:34 -0400

    add test knex connection config object

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const path = require("path")
+
 module.exports = {
   development: {
     client: "pg",
@@ -26,5 +28,14 @@ module.exports = {
       database: "kodede_back_node_production"
     },
     useNullAsDefault: true
-  }
+  },
+
+  test: {
+    client: "sqlite3",
+    connection: ":memory:",
+    useNullAsDefault: true,
+    migrations: {
+      directory: path.join(__dirname, "db", "migrations")
+    },
+  },
 };
