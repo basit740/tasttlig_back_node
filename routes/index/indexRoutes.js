05090e4bc792c83097b6615eb930e9ba80e57a51
Slingr <liquidd2@gmail.com>
2020-06-09 23:12:55 -0400

    configure cors

--- routes/index/indexRoutes.js
+++ routes/index/indexRoutes.js
@@ -3,7 +3,8 @@ const cors = require("cors");
 const indexRouter = require("express").Router();
 
 // Set up CORS
-// indexRouter.use(cors());
+indexRouter.use(cors());
+indexRouter.options("*", cors());
 // indexRouter.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 
 // GET landing page
