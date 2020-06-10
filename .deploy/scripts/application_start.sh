a6ffd029fa6afe4f0e9dcff111ca10eabbe67387
Slingr <liquidd2@gmail.com>
2020-06-09 21:08:36 -0400

    deploy to beanstalk

--- .deploy/scripts/application_start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Stop all servers and start the server
-forever stopall
-forever start /home/ec2-user/kodede/app.js
--- .deploy/scripts/before_install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Install node.js and Forever.js
-sudo yum update
-sudo curl -sL https://rpm.nodesource.com/setup_10.x | sudo bash -
-sudo yum install nodejs -y
-sudo npm install forever -g
