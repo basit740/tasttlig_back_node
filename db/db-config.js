27219d227b23848c77114c97e46a1ba91a6b982f
Slingr <liquidd2@gmail.com>
2020-06-18 22:32:38 -0400

    debugging errors with registration

--- db/db-config.js
+++ db/db-config.js
@@ -1,6 +1,8 @@
 const environment = process.env.NODE_ENV || "development";
 const config = require("../knexfile")[environment];
 
+console.log(config);
+
 const db = require("knex")(config);
 
 db.migrate.latest()
