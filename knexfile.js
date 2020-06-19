cf244d20ca31bcc483a9d0ac56f0e89ec4205a05
Slingr <liquidd2@gmail.com>
2020-06-18 23:30:53 -0400

    debugging errors with registration

--- knexfile.js
+++ knexfile.js
@@ -3,16 +3,43 @@
 const path = require("path")
 
 module.exports = {
-  
+  development: {
+    client: "pg",
+    connection: {
+      database: "kodede_back_node_development"
+    },
+    migrations: {
+      directory: "./db/migrations"
+    },
+    useNullAsDefault: true
+  },
+
+  staging: {
+    client: "pg",
+    connection: {
+      database: "kodede_back_node_staging"
+    },
+    useNullAsDefault: true
+  },
 
   production: {
     client: "pg",
     connection: {
-      database : process.env.RDS_HOSTNAME,
+      host : process.env.RDS_HOSTNAME,
+      dtabase: process.env.RDS_DB_NAME,
       user     : process.env.RDS_USERNAME,
       password : process.env.RDS_PASSWORD,
       port     : process.env.RDS_PORT
     },
     useNullAsDefault: true
+  },
+
+  test: {
+    client: "sqlite3",
+    connection: ":memory:",
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./db/migrations"
+    },
   },
 };
