15517f4d818759a471d0ba8cc35e5680beff31c2
Slingr <liquidd2@gmail.com>
2020-06-19 20:10:53 -0400

    update db config

--- knexfile.js
+++ knexfile.js
@@ -25,11 +25,11 @@ module.exports = {
   production: {
     client: "pg",
     connection: {
-      host : process.env.RDS_HOSTNAME,
-      database: process.env.RDS_DB_NAME,
-      user     : process.env.RDS_USERNAME,
-      password : process.env.RDS_PASSWORD,
-      port     : process.env.RDS_PORT
+      host : process.env.DB_HOSTNAME,
+      database: process.env.DB_DB_NAME,
+      user     : process.env.DB_USERNAME,
+      password : process.env.DB_PASSWORD,
+      port     : process.env.DB_PORT
     },
     migrations: {
       directory: "./db/migrations"
