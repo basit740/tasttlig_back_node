040837161933f1832f0c752f00e8d6d1a63f3e68
Harry Dodoo-Amoo <hdodooamoo@orbiscommunications.com>
2020-06-20 20:59:59 -0400

    add nodemailer config for different environments

--- routes/auth/nodemailer.js
+++ routes/auth/nodemailer.js
@@ -3,12 +3,40 @@
 // Libraries
 const nodemailer = require("nodemailer");
 
-module.exports = {
-  transporter: nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.GMAIL_USER,
-      pass: process.env.GMAIL_PASSWORD
+var mailConfig;
+
+if (process.env.NODE_ENV === 'production' ){
+    mailConfig = {
+      service: "gmail",
+      auth: {
+        user: process.env.GMAIL_USER,
+        pass: process.env.GMAIL_PASSWORD
+      }
+    };
+} else if (process.env.NODE_ENV == 'test') {
+    // use ethereal mail in testing environment
+    // https://ethereal.email/
+    mailConfig = {
+      host: 'smtp.ethereal.email',
+      port: 587,
+      auth: {
+        user: 'keara.block@ethereal.email',
+        pass: 'hbGChvzjwgRwWUeewC'
+      }
     }
-  })
+} else {
+    // use papercut in development
+    // https://github.com/ChangemakerStudios/Papercut-SMTP
+    mailConfig = {
+        host: 'localhost',
+        port: 25,
+        auth: {
+            user: 'user',
+            pass: 'pass'
+        }
+    };
+}
+
+module.exports = {
+  transporter: nodemailer.createTransport(mailConfig)
 };
