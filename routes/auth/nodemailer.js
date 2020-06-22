bc20b11190a4bfb4c83bfdb33eb2b0f94ec8970e
Harry Dodoo-Amoo <hdodooamoo@orbiscommunications.com>
2020-06-21 20:08:26 -0400

    attempt using ses for emailing

--- routes/auth/nodemailer.js
+++ routes/auth/nodemailer.js
@@ -2,17 +2,23 @@
 
 // Libraries
 const nodemailer = require("nodemailer");
+const aws = require('aws-sdk');
 
 var mailConfig;
 
 if (process.env.NODE_ENV === 'production' ){
+    // mailConfig = {
+    //   service: "gmail",
+    //   auth: {
+    //     user: process.env.GMAIL_USER,
+    //     pass: process.env.GMAIL_PASSWORD
+    //   }
+    // };
     mailConfig = {
-      service: "gmail",
-      auth: {
-        user: process.env.GMAIL_USER,
-        pass: process.env.GMAIL_PASSWORD
-      }
-    };
+      SES: new aws.SES({
+          apiVersion: '2010-12-01'
+      })
+  };
 } else if (process.env.NODE_ENV == 'test') {
     // use ethereal mail in testing environment
     // https://ethereal.email/
