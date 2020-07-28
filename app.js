4282e8d3dd317cfa26f30d4459147bace466faf6
rakshit <rakshit1609@gmail.com>
2020-07-28 12:11:13 -0400

    commented out the cors library

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ const user_authentication_router = require('./routes/user/authentication');
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(cors());
-app.options("*", cors());
+// app.use(cors());
+// app.options("*", cors());
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
