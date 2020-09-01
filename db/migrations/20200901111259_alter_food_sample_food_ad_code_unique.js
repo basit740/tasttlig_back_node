8305669d96641e2d7d1d2afaef365c68c05be54f
MingrenChen <chenmr9769@gmail.com>
2020-09-01 13:00:10 -0400

    Update 20200901111259_alter_food_sample_food_ad_code_unique.js

--- db/migrations/20200901111259_alter_food_sample_food_ad_code_unique.js
+++ db/migrations/20200901111259_alter_food_sample_food_ad_code_unique.js
@@ -1,5 +1,15 @@
+const { generateRandomString } = require("../../functions/functions");
+exports.up = async function(knex) {
+  const duplicate_food_ad_code = await knex.raw('select f1.food_sample_id from food_samples as f1, food_samples as f2 ' +
+    'where f1.food_ad_code = f2.food_ad_code and f1.food_sample_id != f2.food_sample_id')
+  for (let i=0; i<duplicate_food_ad_code.rows.length; i++) {
+    const  id = duplicate_food_ad_code.rows[i].food_sample_id
+    await knex('food_samples')
+      .update({food_ad_code: generateRandomString(4)})
+      .where('food_sample_id', '=', id)
 
-exports.up = function(knex) {
+  }
+
   return knex.schema.table("food_samples", table => {
     table.unique('food_ad_code');
   })
