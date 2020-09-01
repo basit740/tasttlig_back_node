b1f2c4b8d01687e553b4bffaa42c260c69ebb81e
Harry Dodoo-Amoo <hdamoo@tasttlig.com>
2020-09-01 20:42:19 +0000

    Resolve conflicts

--- services/authentication/authenticate_user.js
+++ services/authentication/authenticate_user.js
@@ -7,57 +7,80 @@ const Mailer = require("../email/nodemailer").nodemailer_transporter;
 const SITE_BASE = process.env.SITE_BASE;
 
 const userRegister = async (user, sendEmail= true) => {
-  const userData = {
-    first_name: user.first_name,
-    last_name: user.last_name,
-    email: user.email,
-    password: user.password,
-    phone_number: user.phone_number,
-    role: "MEMBER",
-    created_at_datetime: new Date(),
-    updated_at_datetime: new Date()
-  }
-  if (user.is_participating_in_festival){
-    userData.is_participating_in_festival = user.is_participating_in_festival;
-  }
-  return await db("tasttlig_users")
-    .insert(userData)
-    .returning("*")
-    .then(value => {
-      if(sendEmail) {
-        jwt.sign({
-            user: value[0].tasttlig_user_id
-          },
-          process.env.EMAIL_SECRET,
-          {
-            expiresIn: "28d"
-          },
-          // Async email verification email
-          async (err, emailToken) => {
-            try {
-              const urlVerifyEmail = `${SITE_BASE}/user/verify/${emailToken}`;
-              await Mailer.sendMail({
-                from: process.env.SES_DEFAULT_FROM,
-                to: user.email,
-                bcc: process.env.TASTTLIG_ADMIN_EMAIL,
-                subject: "[Tasttlig] Welcome to Tasttlig!",
-                template: 'signup',
-                context: {
-                  first_name: user.first_name,
-                  last_name: user.last_name,
-                  urlVerifyEmail: urlVerifyEmail
-                }
-              });
-            } catch (error) {
-              return {success: false, data: error.message};
+  try{
+    return db.transaction(async trx => {
+      let new_db_user = [];
+      await trx("tasttlig_users")
+        .where("email", user.email)
+        .first()
+        .then(value => {
+          if (!value) {
+            const userData = {
+              first_name: user.first_name,
+              last_name: user.last_name,
+              email: user.email,
+              password: user.password,
+              phone_number: user.phone_number,
+              role: "MEMBER",
+              status: "ACTIVE",
+              created_at_datetime: new Date(),
+              updated_at_datetime: new Date()
             }
+            if (user.is_participating_in_festival) {
+              userData.is_participating_in_festival = user.is_participating_in_festival;
+            }
+            new_db_user = trx("tasttlig_users")
+              .insert(userData)
+              .returning("*");
+          } else {
+            new_db_user = trx("tasttlig_users")
+              .where("tasttlig_user_id", value.tasttlig_user_id)
+              .update({
+                first_name: user.first_name,
+                last_name: user.last_name,
+                password: user.password,
+                phone_number: user.phone_number,
+                role: "MEMBER",
+                status: "ACTIVE",
+                created_at_datetime: new Date(),
+                updated_at_datetime: new Date()
+              })
+              .returning("*");
           }
-        );
-      }
-      return {success: true, data: value[0]};
-    }).catch(reason => {
-      return {success: false, data: reason};
+        });
+      return await new_db_user
+        .then(value1 => {
+          if(sendEmail) {
+            jwt.sign({
+                user: value1[0].tasttlig_user_id
+              },
+              process.env.EMAIL_SECRET,
+              {
+                expiresIn: "28d"
+              },
+              // Async email verification email
+              async (err, emailToken) => {
+                const urlVerifyEmail = `${SITE_BASE}/user/verify/${emailToken}`;
+                await Mailer.sendMail({
+                  from: process.env.SES_DEFAULT_FROM,
+                  to: user.email,
+                  bcc: process.env.TASTTLIG_ADMIN_EMAIL,
+                  subject: "[Tasttlig] Welcome to Tasttlig!",
+                  template: 'signup',
+                  context: {
+                    first_name: user.first_name,
+                    last_name: user.last_name,
+                    urlVerifyEmail: urlVerifyEmail
+                  }
+                });
+              });
+          }
+          return {success: true, data: value1[0]};
+        })
     });
+  }catch (error) {
+    return {success: false, data: error.message};
+  }
 }
 
 const verifyAccount = async user_id => {
