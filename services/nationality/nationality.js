0d471c629e7a1bedf941c8a095ce9635a3cfabc3
Ryota Mine <ryotamine@gmail.com>
2020-09-01 21:23:15 -0400

    format nationality service file

--- services/nationality/nationality.js
+++ services/nationality/nationality.js
@@ -1,19 +1,19 @@
 "use strict";
 
-const {db} = require("../../db/db-config");
+const { db } = require("../../db/db-config");
 
 const getAll = async () => {
-    return await db
-        .select("*")
-        .from("nationalities")
-        .then(value => {
-            return {success: true, nationalities: value};
-        })
-        .catch(reason => {
-            return {success: false, details: reason};
-        });
-}
+  return await db
+    .select("*")
+    .from("nationalities")
+    .then((value) => {
+      return { success: true, nationalities: value };
+    })
+    .catch((reason) => {
+      return { success: false, details: reason };
+    });
+};
 
 module.exports = {
-    getAll
-}
+  getAll,
+};
