480597d79c97602a23d3ef39c62e2f72171af29a
rakshit <rakshit1609@gmail.com>
2020-09-02 00:04:00 -0400

    added passport_id in tasttlig_users table

---
+++ db/migrations/20200901222203_add_passport_id_to_tasttlig_users.js
@@ -0,0 +1,35 @@
+const { generateRandomString } = require("../../functions/functions");
+exports.up = async function(knex) {
+  return knex.select("tasttlig_user_id", "role")
+    .from("tasttlig_users")
+    .then((tasttlig_users) => {
+      const new_tasttlig_users_details = tasttlig_users.map((user) => {
+        if(user.role == "VISITOR") {
+          return { tasttlig_user_id: user.tasttlig_user_id, passport_id: "G" + generateRandomString(6) };
+        } else {
+          return { tasttlig_user_id: user.tasttlig_user_id, passport_id: "M" + generateRandomString(6) };
+        }
+      });
+      return knex.transaction((trx) => {
+        return trx.schema.table("tasttlig_users", (table) => table.string("passport_id"))
+          .then(() => {
+            return Promise.all(
+              new_tasttlig_users_details.map((row) => {
+                return knex('tasttlig_users')
+                  .update({ passport_id: row.passport_id })
+                  .where('tasttlig_user_id', row.tasttlig_user_id)
+                  .transacting(trx);
+              })
+            );
+          })
+          .then(trx.commit)
+          .catch(trx.rollback);
+      });
+    })
+};
+
+exports.down = function(knex) {
+  return knex.schema.table("tasttlig_users", tableBuilder => {
+    tableBuilder.dropColumn("passport_id");
+  })
+};
