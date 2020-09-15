e56ab935ed47e7ac09178a96400d9a2563022149
rakshit <rakshit1609@gmail.com>
2020-09-15 10:15:26 -0400

    creation of query pending

--- services/cron_job/cron_job_functions.js
+++ services/cron_job/cron_job_functions.js
@@ -5,7 +5,25 @@ const {db} = require("../../db/db-config");
 const deleteInactiveItems = async () => {
   try {
     await db.transaction(async trx => {
-      let oldFoodSamples = trx("")
+      // Delete food samples for rejected inactive applications
+      let oldFoodSamples = trx.select("*")
+        .from("applications")
+        .leftJoin(
+          "tasttlig_users",
+          "applications.user_id",
+          "tasttlig_users.tasttlig_user_id"
+        )
+        .leftJoin(
+          "food_samples",
+          "tasttlig_users.tasttlig_user_id",
+          "food_samples.food_sample_creater_user_id"
+        )
+        .leftJoin(
+          "food_sample_images",
+          "food_samples.food_sample_id",
+          "food_sample_images.food_sample_id"
+        )
+        .where("status", "REJECTED").where("status", "")
     });
   }catch (error) {
     return {success: false, details:error.message};
