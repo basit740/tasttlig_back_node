acde23894ea5525b48b14dfc0a871747b13c487e
rakshit <rakshit1609@gmail.com>
2020-09-15 16:38:21 -0400

    cron job function is completed

--- services/cron_job/cron_job_functions.js
+++ services/cron_job/cron_job_functions.js
@@ -2,31 +2,54 @@
 
 const {db} = require("../../db/db-config");
 
+// Delete food samples and menu for rejected inactive applications
 const deleteInactiveItems = async () => {
   try {
+    console.log("Deleting Expired Script Started");
+    const expiryDate = new Date(new Date().setDate(new Date().getDate() - 3));
     await db.transaction(async trx => {
-      // Delete food samples for rejected inactive applications
-      let oldFoodSamples = trx.select("*")
+      let oldApplicationUserIdsQuery = trx.select("user_id")
         .from("applications")
-        .leftJoin(
-          "tasttlig_users",
-          "applications.user_id",
-          "tasttlig_users.tasttlig_user_id"
-        )
-        .leftJoin(
-          "food_samples",
-          "tasttlig_users.tasttlig_user_id",
-          "food_samples.food_sample_creater_user_id"
-        )
-        .leftJoin(
-          "food_sample_images",
-          "food_samples.food_sample_id",
-          "food_sample_images.food_sample_id"
-        )
-        .where("status", "REJECTED").where("status", "")
+        .where("status", "REJECTED")
+        .where("updated_at", "<", expiryDate);
+      
+      let foodSampleIds = trx.select("food_sample_id")
+        .from("food_samples")
+        .whereIn("food_sample_creater_user_id", oldApplicationUserIdsQuery);
+      
+      // Delete food sample images of expired applications
+      let foodSampleImagesDeleteCount = await trx("food_sample_images")
+        .whereIn("food_sample_id", foodSampleIds)
+        .del()
+        .then(deleteCount => {
+          return deleteCount;
+        });
+      
+      // Delete food samples of expired applications
+      let foodSamplesDeleteCount = await trx("food_samples")
+        .whereIn("food_sample_id", foodSampleIds)
+        .del()
+        .then(deleteCount => {
+          return deleteCount;
+        });
+      
+      // Delete menu items of expired applications
+      let menuItemsDeleteCount = await trx("menu_items")
+        .whereIn("menu_item_creator_user_id", oldApplicationUserIdsQuery)
+        .del()
+        .then(deleteCount => {
+          return deleteCount;
+        });
+      
+      console.log("Script executed Successfully");
+      console.log("Food samples deleted: " + foodSamplesDeleteCount);
+      console.log("Food sample images deleted: " + foodSampleImagesDeleteCount);
+      console.log("Menu items deleted: " + menuItemsDeleteCount);
     });
+    return true;
   }catch (error) {
-    return {success: false, details:error.message};
+    console.log("Error in Script: " + error.message);
+    return false;
   }
 }
 
