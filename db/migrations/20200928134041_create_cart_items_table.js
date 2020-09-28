4db9d5ba4e885a59585b28406ef2abf3d788d5af
rakshit <rakshit1609@gmail.com>
2020-09-28 18:11:51 -0400

    save state

---
+++ db/migrations/20200928134041_create_cart_items_table.js
@@ -0,0 +1,18 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable("cart_items", table => {
+    table.increments("cart_item_id").unsigned().primary();
+    table.integer("cart_id").notNullable().index()
+      .references('cart_id').inTable('carts');
+    table.string("status").notNullable();
+    table.string("item_type").notNullable();
+    table.integer("item_id").notNullable();
+    table.integer("quantity").notNullable();
+    table.string("created_at").notNullable();
+    table.string("updated_at").notNullable();
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable("cart_items");
+};
