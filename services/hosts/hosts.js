188f1f8a0526a5c6fa78a2b887396166e219347d
Slingr <liquidd2@gmail.com>
2020-10-20 20:11:46 -0400

    update admin application review page with changes to form

--- services/hosts/hosts.js
+++ services/hosts/hosts.js
@@ -9,7 +9,7 @@ const getHostApplications = async () => {
       .select("*")
       .from("tasttlig_users")
       .innerJoin("business_details", "tasttlig_users.tasttlig_user_id", "business_details.user_id")
-      .where("tasttlig_users.role", "like", "%RESTAURANT_PENDING%");
+      .where("tasttlig_users.role", "like", "%HOST_PENDING%");
     return {
       success: true,
       applications
@@ -21,26 +21,82 @@ const getHostApplications = async () => {
 
 const getHostApplication = async (userId) => {
   try {
-    const application = await db
+    let application = await db
       .select("*")
       .from("tasttlig_users")
       .innerJoin("business_details", "tasttlig_users.tasttlig_user_id", "business_details.user_id")
-      .innerJoin("hosting_application", "tasttlig_users.tasttlig_user_id", "hosting_application.user_id")
-      .innerJoin("payment_bank", "tasttlig_users.tasttlig_user_id", "payment_bank.user_id")
-      .where("tasttlig_users.role", "like", "%RESTAURANT_PENDING%")
+      .innerJoin("payment_info", "tasttlig_users.tasttlig_user_id", "payment_info.user_id")
+      .where("tasttlig_users.role", "like", "%HOST_PENDING%")
       .where("tasttlig_users.tasttlig_user_id", "=", userId)
       .first();
 
     const reviews = await db.select("*")
       .from("external_review")
-      .where("external_review.user_id","=", userId);
+      .where("external_review.user_id", "=", userId);
+
+    reviews.forEach(r => {
+      if (r.text) {
+        application.personal_review = r.text;
+      } else {
+        application[`${r.platform}_review`] = r.link;
+      }
+    })
 
     const documents = await db.select("*")
       .from("documents")
-      .where("documents.user_id","=", userId);
+      .where("documents.user_id", "=", userId);
+
+    const services = await db.select(db.raw("ARRAY_AGG(business_services.name) as names"))
+      .from("business_services")
+      .where("business_services.user_id", "=", userId);
+
+    const menuItems = await db.select(
+      "menu_items.*",
+      db.raw("ARRAY_AGG(menu_item_images.image_url) as image_urls"),
+    )
+      .from("menu_items")
+      .where("menu_items.menu_item_creator_user_id", userId)
+      .leftJoin(
+        "menu_item_images",
+        "menu_items.menu_item_id",
+        "menu_item_images.menu_item_id"
+      )
+      .groupBy("menu_items.menu_item_id")
 
     application.reviews = _.groupBy(reviews, "platform");
     application.documents = documents;
+    application.menu_items = menuItems;
+    application.services = services && services.length ? services[0].names : [];
+
+    const applications = await db.select("*")
+      .from("applications")
+      .where("applications.user_id", "=", userId);
+
+    application.videos = [];
+
+    applications.forEach(a => {
+      if (a.type === "host") {
+        application = {
+          ...application,
+          is_host: "yes",
+          host_selection: a.reason,
+          host_selection_resume: a.resume,
+          host_selection_video: a.video_link,
+          host_youtube_link: a.youtube_link
+        }
+        application.videos.push(a.video_link)
+      } else if (a.type === "cook") {
+        application = {
+          ...application,
+          is_cook: "yes",
+          cook_selection: a.reason,
+          cook_selection_resume: a.resume,
+          cook_selection_video: a.video_link,
+          cook_youtube_link: a.youtube_link
+        }
+        application.videos.push(a.video_link)
+      }
+    })
 
     return {
       success: true,
