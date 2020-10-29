f71a62cb4e15351eaefe3399784450731a352045
Slingr <liquidd2@gmail.com>
2020-10-28 22:46:10 -0400

    add new fields to assets table

--- services/assets/assets.js
+++ services/assets/assets.js
@@ -9,8 +9,18 @@ const addAsset = async (
   try {
     const asset = {
       name: assetDetails.name,
+      type: assetDetails.asset_type,
+      price: assetDetails.asset_price,
       description: assetDetails.description,
+      contact_name: assetDetails.asset_contact,
+      contact_phone: assetDetails.asset_contact_phone,
+      start_time: new Date(assetDetails.asset_start_time).toLocaleTimeString(),
+      end_time: new Date(assetDetails.asset_end_time).toLocaleTimeString(),
       user_id: db_user.tasttlig_user_id
+    }
+
+    if (assetDetails.asset_availability) {
+      asset.availability = assetDetails.asset_availability.join(",")
     }
 
     const a = await trx("assets").insert(asset).returning("*");
