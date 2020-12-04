595e6b7e68100b77da7360ff2980ad5522d9c9d0
rakshit <rakshit1609@gmail.com>
2020-12-04 00:22:11 -0500

    added kodidi external api to display user specials

--- routes/external_api/external_api.js
+++ routes/external_api/external_api.js
@@ -2,10 +2,23 @@
 
 const router = require('express').Router();
 const external_api_service = require('../../services/external_api_service');
+const token_service = require("../../services/authentication/token");
 
 router.get("/kodidi/specials_list", async (req, res) => {
   try {
     const response = await external_api_service.getKodidiSpecialsList();
+    return res.send(response);
+  } catch (e) {
+    res.send({
+      success: false,
+      message: e.message,
+    });
+  }
+});
+
+router.get("/kodidi/user_specials_list", token_service.authenticateToken, async (req, res) => {
+  try {
+    const response = await external_api_service.getKodidiUserSpecialsList(req.user.email);
     return res.send(response);
   } catch (e) {
     res.send({
