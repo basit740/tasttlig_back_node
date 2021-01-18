9a8d5a2b55f61ba2eba16039333374c8e9a3f3c8
Thilakshan <th_thilakshan@hotmail.com>
2021-01-18 10:52:30 -0500

    fixed small changes

--- routes/festival/festival.js
+++ routes/festival/festival.js
@@ -1,11 +1,6 @@
 "use strict";
 // Libraries
 const router = require("express").Router();
-const token_service = require("../../services/authentication/token");
-const food_sample_claim_service = require("../../services/food_sample_claim/food_sample_claim");
-const user_profile_service = require("../../services/profile/user_profile");
-const food_sample_service = require("../../services/food_sample/food_sample");
-const authenticate_user_service = require("../../services/authentication/authenticate_user");
 const festival_service = require("../../services/festival/festival");
 
 router.get("/get-festivals", async (req, res) => {
