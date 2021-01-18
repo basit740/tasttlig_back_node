64387bc875d6297c0b63b5070375f6a99d4f367a
Ryota Mine <ryotamine@gmail.com>
2021-01-18 18:40:19 -0500

    updated create festival route and service to align with the form

--- routes/festival/festival.js
+++ routes/festival/festival.js
@@ -1,17 +1,98 @@
 "use strict";
+
 // Libraries
 const router = require("express").Router();
+const token_service = require("../../services/authentication/token");
 const festival_service = require("../../services/festival/festival");
+const user_profile_service = require("../../services/profile/user_profile");
 
 router.get("/get-festivals", async (req, res) => {
   const festivals = await festival_service.getAllFestivals();
   return res.send(festivals);
 });
 
-router.post("/festival/add", async (req, res) => {
-  const newFestival = await festival_service.createNewFestival(req.body);
-  console.log(req.body);
-  return res.send(newFestival);
-})
+// POST festival
+router.post(
+  "/festival/add",
+  token_service.authenticateToken,
+  async (req, res) => {
+    const {
+      images,
+      festival_name,
+      festival_type,
+      festival_price,
+      festival_city,
+      festival_start_date,
+      festival_end_date,
+      festival_start_time,
+      festival_end_time,
+      festival_image_description,
+    } = req.body;
+
+    try {
+      if (
+        !images ||
+        !festival_name ||
+        !festival_type ||
+        !festival_price ||
+        !festival_city ||
+        !festival_start_date ||
+        !festival_end_date ||
+        !festival_start_time ||
+        !festival_end_time ||
+        !festival_image_description
+      ) {
+        return res.status(403).json({
+          success: false,
+          message: "Required parameters are not available in request.",
+        });
+      }
+
+      try {
+        const user_details_from_db = await user_profile_service.getUserById(
+          req.user.id
+        );
+
+        if (!user_details_from_db.success) {
+          return res.status(403).json({
+            success: false,
+            message: user_details_from_db.message,
+          });
+        }
+
+        const festival_details = {
+          festival_name,
+          festival_type,
+          festival_price: parseInt(festival_price),
+          festival_city,
+          festival_start_date: festival_start_date.substring(0, 10),
+          festival_end_date: festival_end_date.substring(0, 10),
+          festival_start_time,
+          festival_end_time,
+        };
+
+        const response = await festival_service.createNewFestival(
+          festival_details,
+          images,
+          festival_image_description
+        );
+
+        return res.send(response);
+      } catch (error) {
+        res.send({
+          success: false,
+          message: "Error.",
+          response: error,
+        });
+      }
+    } catch (error) {
+      res.send({
+        success: false,
+        message: "Error.",
+        response: error,
+      });
+    }
+  }
+);
 
 module.exports = router;
--- services/festival/festival.js
+++ services/festival/festival.js
@@ -4,56 +4,45 @@
 const { db } = require("../../db/db-config");
 
 const getAllFestivals = async () => {
-  let query = db.
-  select("*")
-  .from("festivals");
-  return await query
-  .then((value) => {
+  let query = db.select("*").from("festivals");
+  return await query.then((value) => {
     return { success: true, details: value };
-  })
-}
+  });
+};
 
-const createNewFestival = async(details) => {
-  console.log("hello");
+/* Save new festival to festivals and festival images tables helper 
+function */
+const createNewFestival = async (
+  festival_details,
+  festival_images,
+  festival_image_description
+) => {
   try {
     await db.transaction(async (trx) => {
-      /* if (
-        createdByAdmin ||
-        user_role_object.includes("RESTAURANT") ||
-        user_role_object.includes("RESTAURANT_PENDING")
-      ) {
-        food_sample_details.status = "ACTIVE";
-      } */
-      const festival_details = {
-        festival_start_time: details.festival_start_time,
-        festival_end_time: details.festival_end_time,
-        festival_name: details.festival_name,
-        festival_type: details.festival_type,
-        festival_price: details.festival_price,
-        festival_city: details_festival_city,
-        festival_start_date: details.festival_start_date,
-        festival_end_date: details.festival_end_date,
+      const db_festival = await trx("festivals")
+        .insert(festival_details)
+        .returning("*");
+
+      if (!db_festival) {
+        return { success: false, details: "Inserting new festival failed." };
       }
-      console.log(db_festival);
-      console.log(festival_details);
-      const db_festival = await trx("festival")
-      .insert(festival_details)
-      .returning("*")
-    })
-    const images = details.festival_images.map((festival_image) => ({
-      festival_id: db_festival[0].festival_id,
-      image_url: festival_image,
-      description: details.festival_image_description
-    }));
 
-    await trx("festival_images").insert(images);
+      const images = festival_images.map((festival_image_url) => ({
+        festival_id: db_festival[0].festival_id,
+        festival_image_url,
+        festival_image_description,
+      }));
 
-   } catch(error) {
-      return { success: false, details: error.message };
-    }
+      await trx("festival_images").insert(images);
+    });
+
+    return { success: true, details: "Success." };
+  } catch (error) {
+    return { success: false, details: error.message };
   }
+};
 
 module.exports = {
   getAllFestivals,
-  createNewFestival
+  createNewFestival,
 };
