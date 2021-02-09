8e6edf95b0e8b898d9d87f9657e7844a5d7241ee
Princeton Arulnesan <princeton.arul@gmail.com>
2021-02-09 15:14:56 -0500

    migration fixes

--- db/migrations/20210205133145_update2_vendors_table.js
+++ db/migrations/20210205133145_update2_vendors_table.js
@@ -4,7 +4,7 @@ exports.up = function(knex) {
       
       
       tableBuilder.boolean("is_available_on_monday_for_festival");
-      tableBuilder.boolean("is_available_on_tuedday_for_festival");
+      tableBuilder.boolean("is_available_on_tuesday_for_festival");
       tableBuilder.boolean("is_available_on_wednesday_for_festival");
       tableBuilder.boolean("is_available_on_thursday_for_festival");
       tableBuilder.boolean("is_available_on_friday_for_festival");
@@ -18,7 +18,7 @@ exports.up = function(knex) {
     return knex.schema.table("vendors", tableBuilder => {
       
         tableBuilder.dropColumn("is_available_on_monday_for_festival");
-        tableBuilder.dropColumn("is_available_on_tuedday_for_festival");
+        tableBuilder.dropColumn("is_available_on_tuesday_for_festival");
         tableBuilder.dropColumn("is_available_on_wednesday_for_festival");
         tableBuilder.dropColumn("is_available_on_thursday_for_festival");
         tableBuilder.dropColumn("is_available_on_friday_for_festival");
