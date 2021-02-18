854f8bf28255d4d072659ff7e25448c708e692c7
Sandeep Nannamu <snannamu@bitbucket.org>
2021-02-18 13:00:00 -0500

    updated food-sample table

---
+++ db/migrations/20210218124056_update_festival+selected_in_food_sample_table.js
@@ -0,0 +1,30 @@
+exports.up = function(knex) {
+    return knex.schema.alterTable("food_samples", table => {
+           
+      
+        
+        table.specificType("festival_selected", 'VARCHAR[]');
+           
+           
+      
+    });
+  };
+  
+  exports.down = function(knex) {
+    return knex.schema.alterTable("food_samples", table => {
+        
+        
+        table.dropColumn("festival_selected");
+        
+         
+        
+    });
+  };
+
+
+
+
+  
+
+
+
