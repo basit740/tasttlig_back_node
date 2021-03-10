6a76867e3be66fbc080bad71f56e2112b117159f
Princeton Arulnesan <princeton.arul@gmail.com>
2021-03-09 20:32:09 -0500

    added new routes for some of the tabs in the dashboard

---
+++ routes/order/order.js
@@ -0,0 +1,44 @@
+"use strict";
+
+// Libraries
+const router = require("express").Router();
+const stripe_payment_service = require("../../services/payment/stripe_payment");
+const order_service = require("../../services/order/order");
+const authenticate_user_service = require("../../services/authentication/authenticate_user");
+const user_profile_service = require("../../services/profile/user_profile");
+const food_sample_claim_service = require("../../services/food_sample_claim/food_sample_claim");
+const token_service = require("../../services/authentication/token");
+
+
+router.get("/orders/user/:userId", token_service.authenticateToken,  async (req, res) => {
+  try {
+    const userOrders = await order_service.getAllUserOrders(
+      req.user.id
+    );
+
+    return res.send(userOrders);
+  } catch (error) {
+    console.log(error)
+    res.status(500).send({
+      success: false,
+      message: error.message,
+    });
+  }
+})
+router.get("/orders/current/user/:userId", token_service.authenticateToken,  async (req, res) => {
+  try {
+    const userOrders = await order_service.getAllCurrentOrders(
+      req.user.id
+    );
+
+    return res.send(userOrders);
+  } catch (error) {
+    console.log(error)
+    res.status(500).send({
+      success: false,
+      message: error.message,
+    });
+  }
+})
+
+module.exports = router;
