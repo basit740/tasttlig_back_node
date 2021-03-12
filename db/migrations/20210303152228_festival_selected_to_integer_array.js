e393a47e1d3c8a7049a20522ae516a48e1ca7e7e
Princeton Arulnesan <princeton.arul@gmail.com>
2021-03-12 18:46:27 -0500

    fixes to migrations

--- db/migrations/20210303152228_festival_selected_to_integer_array.js
+++ db/migrations/20210303152228_festival_selected_to_integer_array.js
@@ -1,5 +1,6 @@
 
-exports.up = function(knex) {
+exports.up = async function(knex) {
+  await knex.raw("update food_samples set festival_selected = null")
   return knex.schema.alterTable("food_samples", table => {
     table.specificType("festival_selected", 'INT[]').alter();
   
