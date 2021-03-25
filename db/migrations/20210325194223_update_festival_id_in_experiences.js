47eb0621780b3892f2bad8fc0f1454cf05051c69
Sandeep Nannamu <snannamu@bitbucket.org>
2021-03-25 19:48:16 -0400

    updated experiences tables

---
+++ db/migrations/20210325194223_update_festival_id_in_experiences.js
@@ -0,0 +1,15 @@
+exports.up = function(knex) {
+    return knex.schema.alterTable("experiences", table => {
+      
+      table.specificType("festival_selected", 'VARCHAR[]');
+
+    });
+  };
+  exports.down = function(knex) {
+    return knex.schema.alterTable("experiences", table => {
+        
+        table.dropColumn("festival_selected");
+        
+        
+    });
+  };
