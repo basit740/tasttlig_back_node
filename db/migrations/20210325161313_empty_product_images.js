8c71d21f7f1bc9874934631a039971c9f7d47d10
Sandeep Nannamu <snannamu@bitbucket.org>
2021-03-25 17:08:38 -0400

    deleted old products table

---
+++ db/migrations/20210325161313_empty_product_images.js
@@ -0,0 +1,15 @@
+
+exports.up = function (knex){
+    return knex("product_images")
+    .where('product_image_id', '>', 0)
+    .del()
+
+  };
+    
+
+exports.down = function (knex) {
+    return knex.schema.table("product_image_id", () => {});
+  };
+
+
+  
