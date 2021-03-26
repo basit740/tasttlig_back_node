8c80bcc9352f75507ec0941657232e77e854b1fc
Sandeep Nannamu <snannamu@bitbucket.org>
2021-03-26 15:43:40 -0400

    updated basic full stack functionality for services form

--- db/migrations/20210220175130_drop_old_passport link in passport_images.js
+++ db/migrations/20210220175130_drop_old_passport link in passport_images.js
@@ -1,11 +1,11 @@
 exports.up = function (knex) {
   return knex.schema.table("passport_images", (tableBuilder) => {
-    tableBuilder.dropColumn("passport_id");
+    // tableBuilder.dropColumn("passport_id");
   });
 };
 
 exports.down = function (knex) {
   return knex.schema.table("passport_images", (tableBuilder) => {
-    tableBuilder.integer("passport_id");
+    // tableBuilder.integer("passport_id");
   });
 };
