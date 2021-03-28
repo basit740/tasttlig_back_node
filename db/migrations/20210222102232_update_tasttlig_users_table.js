231ad4a7afa9d54aa8cfbe2d1730b1b9a1aae526
Princeton Arulnesan <princeton.arul@gmail.com>
2021-03-28 18:08:12 -0400

    merged branch

--- db/migrations/20210222102232_update_tasttlig_users_table.js
+++ db/migrations/20210222102232_update_tasttlig_users_table.js
@@ -1,15 +1,15 @@
 exports.up = function (knex) {
   return knex.schema.alterTable("tasttlig_users", (table) => {
-    // table.specificType("preferred_country_cuisine", "VARCHAR[]");
-    // table.specificType("food_preferences", "VARCHAR[]");
-    // table.specificType("food_allergies", "VARCHAR[]");
+    table.specificType("preferred_country_cuisine", "VARCHAR[]");
+    table.specificType("food_preferences", "VARCHAR[]");
+    table.specificType("food_allergies", "VARCHAR[]");
   });
 };
 
 exports.down = function (knex) {
   return knex.schema.alterTable("tasttlig_users", (table) => {
-    // table.dropColumn("preferred_country_cuisine");
-    // table.dropColumn("food_preferences");
-    // table.dropColumn("food_allergies");
+    table.dropColumn("preferred_country_cuisine");
+    table.dropColumn("food_preferences");
+    table.dropColumn("food_allergies");
   });
 };
