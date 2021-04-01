9c2d9580a8acfaa5534d1b3f994855e8d7933e21
Sandeep Nannamu <snannamu@bitbucket.org>
2021-04-01 14:43:14 -0400

    added created_by_admin for users

---
+++ db/migrations/20210401144043_add_crated_by_admin_in_users.js
@@ -0,0 +1,11 @@
+
+exports.up = async function(knex) {
+    return knex.schema.alterTable("tasttlig_users", table => {
+      table.boolean("created_by_admin")
+  });
+  };
+  exports.down = function(knex) {
+    return knex.schema.alterTable("tasttlig_users", table => {
+    table.dropColumn("created_by_admin")
+    })
+  };
