0b9f6c93f85127aa94b62d6f7a6837da0e85b057
Sher Shah Arsalaie <shershah.arsalaie@hotmail.com>
2021-05-12 13:12:18 -0700

    fixing production changes

--- db/migrations/20210220225529_add_more_columns_to_business_details_table.js
+++ db/migrations/20210220225529_add_more_columns_to_business_details_table.js
@@ -1,25 +1,25 @@
 exports.up = function (knex) {
   return knex.schema.alterTable("business_details", (table) => {
-    table.string("business_passport_id");
-    table.string("business_street_number");
-    table.string("business_street_name");
-    table.string("business_unit");
-    table.boolean("business_registered");
-    table.boolean("retail_business");
-    table.string("food_business_type");
-    table.string("food_handlers_certificate");
+    // table.string("business_passport_id");
+    // table.string("business_street_number");
+    // table.string("business_street_name");
+    // table.string("business_unit");
+    // table.boolean("business_registered");
+    // table.boolean("retail_business");
+    // table.string("food_business_type");
+    // table.string("food_handlers_certificate");
   });
 };
 
 exports.down = function (knex) {
   return knex.schema.alterTable("business_details", (table) => {
-    table.dropColumn("business_passport_id");
-    table.dropColumn("business_street_number");
-    table.dropColumn("business_street_name");
-    table.dropColumn("business_unit");
-    table.dropColumn("business_registered");
-    table.dropColumn("retail_business");
-    table.dropColumn("food_business_type");
-    table.dropColumn("food_handlers_certificate");
+    // table.dropColumn("business_passport_id");
+    // table.dropColumn("business_street_number");
+    // table.dropColumn("business_street_name");
+    // table.dropColumn("business_unit");
+    // table.dropColumn("business_registered");
+    // table.dropColumn("retail_business");
+    // table.dropColumn("food_business_type");
+    // table.dropColumn("food_handlers_certificate");
   });
 };
