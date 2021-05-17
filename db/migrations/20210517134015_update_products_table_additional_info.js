2cc33359094ac53ca19d4566869774ddc60a4bd1
Sandeep Nannamu <snannamu@bitbucket.org>
2021-05-17 13:43:07 -0400

    adding updating product table with additional info

---
+++ db/migrations/20210517134015_update_products_table_additional_info.js
@@ -0,0 +1,12 @@
+
+exports.up = function(knex) {
+    return knex.schema.alterTable("products", table => {
+      table.text("additional_information");
+    });
+  };
+  
+  exports.down = function(knex) {
+    return knex.schema.alterTable("products", table => {
+        table.dropColumn("additional_information");
+    });
+  };
