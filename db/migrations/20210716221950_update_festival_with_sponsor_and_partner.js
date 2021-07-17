c906385d9c0da126afd47162d65139ad19a160f3
Fan Yang <frankyang1207@gmail.com>
2021-07-16 22:26:59 -0400

    migration

---
+++ db/migrations/20210716221950_update_festival_with_sponsor_and_partner.js
@@ -0,0 +1,19 @@
+exports.up = function(knex) {
+    return knex.schema.alterTable("festivals", table => {
+      
+      table.specificType("sponsor_request_id", 'INT[]');
+      table.specificType("partner_request_id", 'INT[]');
+      table.specificType("festival_business_partner_id", 'INT[]');
+      
+    });
+  };
+  
+  exports.down = function(knex) {
+    return knex.schema.alterTable("festivals", table => {
+        
+        table.dropColumn("sponsor_request_id");
+        table.dropColumn("partner_request_id");
+        table.dropColumn("festival_business_partner_id");
+    });
+  };
+  
