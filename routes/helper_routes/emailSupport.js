f55d779b0dc142fef37b010141302506163141e9
michellanet <michellanet@gmail.com>
2021-08-24 13:18:51 -0400

    host support update

--- routes/helper_routes/emailSupport.js
+++ routes/helper_routes/emailSupport.js
@@ -2,22 +2,27 @@
 
 // Libraries
 const router = require("express").Router();
+const token_service = require("../../services/authentication/token");
 const {
   emailSupportService,
 } = require("../../services/email/emailSupportService");
 
 // route
-router.post("/email-service", async (req, res) => {
-  try {
-    const response = await emailSupportService(req.body);
-
-    return res.send({ success: true, message: response });
-  } catch (error) {
-    res.send({
-      success: false,
-      message: error,
-    });
+router.post(
+  "/email-service",
+  token_service.authenticateToken,
+  async (req, res) => {
+    try {
+      const response = await emailSupportService(req.body);
+      console.log("sendres", response);
+      return res.send({ success: response.success, message: response });
+    } catch (error) {
+      res.send({
+        success: false,
+        message: error,
+      });
+    }
   }
-});
+);
 
 module.exports = router;
