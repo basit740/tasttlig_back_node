bc3f92bf14d0d61a3950904e30b6483e8fce95e2
Fan Yang <frankyang1207@gmail.com>
2021-09-28 22:27:49 -0400

    test server business list fix attempt #2

--- routes/festival/festival.js
+++ routes/festival/festival.js
@@ -621,7 +621,7 @@ router.post(
           images
         );
         console.log("response from festival/add:", response);
-          console.log("business_list:", festival_participating_business)
+
         // insert the business list into buiness table
         if (festival_participating_business) {
           const business_arr = festival_participating_business.split("|");
@@ -632,7 +632,7 @@ router.post(
               business_arr[i+3],
               business_arr[i+4],
             );
-
+             console.log("123456", business_response.details[0]);
             await festival_service.addBusinessInFestival(response.details, business_response.details[0])
           }
           
