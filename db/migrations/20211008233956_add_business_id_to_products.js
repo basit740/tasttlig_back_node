771578b1ab5be4dbe651753b20b5147736787733
Andre John Louis Cura <acura@tasttlig.com>
2021-10-12 12:53:08 -0400

    added product_business_id if it does not exist

--- db/migrations/20211008233956_add_business_id_to_products.js
+++ db/migrations/20211008233956_add_business_id_to_products.js
@@ -1,21 +1,14 @@
-exports.up = function (knex) {
-  return knex.schema.table("products", (table) => {
-    table.integer("product_business_id");
-  }).then(async () => {
-    await knex.raw(`UPDATE products p SET product_business_id = business_details_id FROM business_details b WHERE b.business_details_user_id=p.product_user_id`);
-    return knex.schema.table("products", (t) => {
-      t.dropColumn("product_user_id")
-    })
-  });
+exports.up = async function (knex) {
+  const exists = await knex.schema.hasColumn("products", "product_business_id");
+  if (!exists) {
+    return knex.schema.table("products", (table) => {
+      table.integer("product_business_id");
+    });
+  }
 };
 
 exports.down = function (knex) {
   return knex.schema.table("products", (table) => {
-    table.integer("product_user_id");
-  }).then(async () => {
-    await knex.raw(`UPDATE products p SET product_user_id = business_details_user_id FROM business_details b WHERE b.business_details_id=p.product_business_details_id`);
-    return knex.schema.table("products", (t) => {
-      t.dropColumn("product_business_id")
-    })
+    table.dropColumn("product_business_id");
   });
 };
