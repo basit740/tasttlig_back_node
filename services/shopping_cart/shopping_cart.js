c327cda1e4aa60e340e3f432d77ba122c2a1531a
Fan Yang <frankyang1207@gmail.com>
2022-05-11 08:57:38 -0400

    fixed cart service comments

--- services/shopping_cart/shopping_cart.js
+++ services/shopping_cart/shopping_cart.js
@@ -31,7 +31,7 @@ const createCart = async (user_id) => {
     });
 };
 
-// Add item to shopping cart helper function
+// Add a cart item to shopping cart helper function
 const addCartItem = async (user_id, cart_id, item_type, item_id, quantity, amount) => {
   return await db("cart_items")
     .insert({
@@ -50,7 +50,7 @@ const addCartItem = async (user_id, cart_id, item_type, item_id, quantity, amoun
     });
 };
 
-// Get an item using cart item id helper function
+// Get a cart item using cart item id helper function
 const getCartItem = async (cart_id) => {
   return await db("cart_items")
     .where("cart_items.cart_item_id", cart_id)
@@ -62,7 +62,7 @@ const getCartItem = async (cart_id) => {
     });
 };
 
-// Delete an item from shopping cart helper function
+// Delete a cart item from shopping cart helper function
 const deleteCartItem = async (user_id, cart_item_id) => {
   await getCartItem(cart_item_id);
   return await db("cart_items")
@@ -79,7 +79,7 @@ const deleteCartItem = async (user_id, cart_item_id) => {
     });
 };
 
-// Delete all items from a shopping cart helper function
+// Delete all cart items from a shopping cart helper function
 const deleteAllCartItem = async (user_id) => {
   return await db("cart_items")
     .where({
