7cbf6dff3774d74d9600e993bf14b092175dbe3e
Slingr <liquidd2@gmail.com>
2022-05-31 18:06:45 -0400

    add endpoint to retrieve all user orders

--- services/order/order_service.js
+++ services/order/order_service.js
@@ -5,72 +5,16 @@ const {db} = require("../../db/db-config");
 const {retrieveOrderItem} = require("./order_item_retriever");
 const Orders = require("../../models/orders")
 
-const getAllUserOrders = async (user_id) => {
-  let query = db
-    .select(
-      "orders.*",
-      "order_items.*",
-      "products.title",
-      "services.service_name",
-      "f1.festival_name AS product_festival",
-      "f2.festival_name AS service_festival"
-    )
-    .from("order_items")
-    .leftJoin("orders", "order_items.order_id", "orders.order_id")
-    .leftJoin(
-      "products",
-      db.raw("CAST(order_items.item_id AS INT)"),
-      "products.product_id"
-    )
-    .leftJoin(
-      "services",
-      db.raw("CAST(order_items.item_id AS INT)"),
-      "services.service_id"
-    )
-    .leftJoin(
-      "festivals AS f1",
-      "products.festival_selected[1]",
-      "f1.festival_id"
-    )
-    .leftJoin(
-      "festivals AS f2",
-      "services.festivals_selected[1]",
-      "f2.festival_id"
-    )
-    .groupBy(
-      "orders.*",
-      "order_items.*",
-      "orders.order_id",
-      "order_items.order_id",
-      "order_items.order_item_id",
-      "products.product_id",
-      "products.title",
-      "products.festival_selected[1]",
-      "services.service_id",
-      "services.service_name",
-      "services.festivals_selected[1]",
-      "f1.festival_name",
-      "f2.festival_name",
-      "f1.festival_id",
-      "f2.festival_id"
-    )
-    .where("orders.order_by_user_id", "=", user_id)
-    .andWhere(function () {
-      this.where("order_items.item_type", "product")
-        .orWhere("order_items.item_type", "service")
-        .orWhere("order_items.item_type", "festival");
-      //.orWhere("order_items.subscription_code", "S_C3")
-    });
-
-  return await query
-    .then((value) => {
-      console.log(value);
-      return {success: true, details: value};
-    })
-    .catch((reason) => {
-      console.log(reason);
-      return {success: false, details: reason};
-    });
+const getUserOrders = async (userId) => {
+  try {
+    const orders = await Orders.query()
+      .where({order_by_user_id: userId})
+      .withGraphFetched("[order_items, payments]")
+    return {success: true, orders};
+  } catch (e) {
+    console.error(e);
+    return {success: false, message: e.message};
+  }
 };
 const getAllCurrentOrders = async (user_id) => {
   let query = db
@@ -203,7 +147,7 @@ function calculateTax(amount) {
 }
 
 module.exports = {
-  getAllUserOrders,
+  getUserOrders,
   getAllCurrentOrders,
   createOrder,
   getOrderForPaymentIntent
