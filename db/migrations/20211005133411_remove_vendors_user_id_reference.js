ee74c0922e2810ea2b894b32be2fa61500839351
Basit Ali <alib4111@gmail.com>
2024-01-19 04:44:23 +0500

    vendors_user_ids_reference commented

--- db/migrations/20211005133411_remove_vendors_user_id_reference.js
+++ db/migrations/20211005133411_remove_vendors_user_id_reference.js
@@ -1,18 +1,19 @@
 exports.up = function (knex) {
-    return knex.schema.alterTable("vendors", (table) => {
-        table.dropForeign('vendor_user_id');
-        table.dropForeign('vendor_business_id');
-    });
-  };
-  
-  exports.down = function (knex) {
-    return knex.schema.alterTable("vendors", (table) => {
-      table.foreign('vendor_user_id')
-      .references("business_details_user_id")
-      .inTable("business_details");
-      table.foreign('vendor_business_id')
-      .references("business_details_id")
-      .inTable("business_details");
-  });
-  };
-  
+	return knex.schema.alterTable('vendors', (table) => {
+		// table.dropForeign('vendor_user_id');
+		// table.dropForeign('vendor_business_id');
+	});
+};
+
+exports.down = function (knex) {
+	return knex.schema.alterTable('vendors', (table) => {
+		table
+			.foreign('vendor_user_id')
+			.references('business_details_user_id')
+			.inTable('business_details');
+		table
+			.foreign('vendor_business_id')
+			.references('business_details_id')
+			.inTable('business_details');
+	});
+};
