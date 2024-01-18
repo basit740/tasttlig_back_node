7e7e20850ac04790e4dd8d2cb00721a47113cbd9
Basit Ali <alib4111@gmail.com>
2024-01-19 04:03:29 +0500

    before updating knex version

--- db/migrations/20210222105931_remove_columns_from_hosts_table.js
+++ db/migrations/20210222105931_remove_columns_from_hosts_table.js
@@ -1,13 +1,14 @@
+// Reason for current error
 exports.up = function (knex) {
-  return knex.schema.table("hosts", (table) => {
-    table.dropColumn("cuisine_type");
-    table.dropColumn("has_hosted_anything_before");
-  });
+	// return knex.schema.table("hosts", (table) => {
+	//   table.dropColumn("cuisine_type");
+	//   table.dropColumn("has_hosted_anything_before");
+	// });
 };
 
 exports.down = function (knex) {
-  return knex.schema.table("hosts", (table) => {
-    table.string("cuisine_type");
-    table.boolean("has_hosted_anything_before");
-  });
+	// return knex.schema.table("hosts", (table) => {
+	//   table.string("cuisine_type");
+	//   table.boolean("has_hosted_anything_before");
+	// });
 };
--- db/migrations/20210329162037_alter_column_promotion_name.js
+++ db/migrations/20210329162037_alter_column_promotion_name.js
@@ -1,10 +1,10 @@
 exports.up = function (knex) {
-  return knex.schema.alterTable("promotions", (table) => {
-    table.renameColumn("promtion_name", "promotion_name");
-  });
+	// return knex.schema.alterTable("promotions", (table) => {
+	//   table.renameColumn("promtion_name", "promotion_name");
+	// });
 };
 exports.down = function (knex) {
-  return knex.schema.alterTable("promotions", (table) => {
-    table.renameColumn("promotion_name", "promtion_name");
-  });
+	// return knex.schema.alterTable("promotions", (table) => {
+	//   table.renameColumn("promotion_name", "promtion_name");
+	// });
 };
